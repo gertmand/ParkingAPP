de2491ea80b19f8298d6577e72bb8a082b18df46
gertmand <gertmand1@gmail.com>
2021-04-05 15:19:56 +0300

    Update RenderView.tsx

--- reactApp/src/services/settings/RenderView.tsx
+++ reactApp/src/services/settings/RenderView.tsx
@@ -2,16 +2,17 @@ import { CircularProgress } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store';
+import { User } from '../../store/types/userType';
 import GlobalStyles from '../../style/GlobalStyles';
 import PrivateRoutes from './PrivateRoutes';
 import Routes from './Routes';
 
 const RenderView = (enterprise: any) => {
-    //const UserData = useSelector<AppState, User>(state => state.user.userData);
+    const UserData = useSelector<AppState, User>(state => state.user.userData);
     const userDataFetching = useSelector<AppState, boolean>(state => state.user.enterpriseUserDataFetching);
     const parkingSpotFetching = useSelector<AppState, boolean>(state => state.user.enterpriseParkingSpotDataFetching);
 
-    if (!userDataFetching && !parkingSpotFetching) {
+    if (UserData && !userDataFetching && !parkingSpotFetching) {
         if (!localStorage.getItem('token')) {
         return (
             <>
