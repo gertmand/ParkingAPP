8c14e355301950aa3b2e010cb4a74af20d1cf402
tmeier@ttu.ee <tmeier@ttu.ee>
2021-04-09 21:32:13 +0300

    Frondis parklapildi kuvamine, API pool tegemata

--- reactApp/src/components/enterprise/Admin/Parking/parkingTable.tsx
+++ reactApp/src/components/enterprise/Admin/Parking/parkingTable.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, InputAdornment, SvgIcon, TextField, Tooltip } from '@material-ui/core';
+import { Backdrop, Box, Button, ButtonGroup, CardMedia, createStyles, Fade, InputAdornment, makeStyles, Modal, SvgIcon, TextField, Theme, Tooltip } from '@material-ui/core';
 import { DataGrid, GridColumns, GridValueGetterParams } from '@material-ui/data-grid';
 import React, { FC, useState } from 'react';
 import { PlusCircle, Search as SearchIcon, XCircle } from 'react-feather';
@@ -16,8 +16,7 @@ const ParkingTable:FC<TableProps>  = ({parkingSpots, updateParkingSpots}) => {
     const dispatch = useDispatch();
     const[searchTerm, setSearchTerm] = useState('')
 
-
-    function getFullName(params: GridValueGetterParams) {
+    function getParkingSpotId(params: GridValueGetterParams) {
       return `${params.getValue('id')}`;
     }
 
@@ -26,26 +25,57 @@ const ParkingTable:FC<TableProps>  = ({parkingSpots, updateParkingSpots}) => {
         { field: 'number', headerName: 'Parklakoha number', width: 190, align : 'center', headerAlign : 'center'},
         { field: 'mainUser', headerName: 'Peakasutaja(d)', width:200, headerAlign : 'center' },
         { field: 'carNumber', headerName: ' Auto reg. number', width: 200, headerAlign : 'center'},
-        { field: "tegevused", headerName: "Tegevused", sortable: false, width: 150, disableClickEventBubbling: true, headerAlign : 'center', valueGetter:getFullName,
+        { field: "tegevused", headerName: "Tegevused", sortable: false, width: 150, disableClickEventBubbling: true, headerAlign : 'center', valueGetter:getParkingSpotId,
             renderCell: (params : GridValueGetterParams) => {
               return <ButtonGroup>
                 <Tooltip title="Lisa peakasutaja"><Button><PlusCircle color="#77d18f"/></Button></Tooltip>
-                <Tooltip title="Kustuta parkimiskoht"><Button onClick={() => deleteParkingSpot(+getFullName(params))?.then(()=>{updateParkingSpots(); dispatch(SET_SUCCESS_ALERT({status:true, message: "Parkimiskoht kustutatud!"}));})}><XCircle color="#e08d8d"/></Button></Tooltip>
+                <Tooltip title="Kustuta parkimiskoht"><Button onClick={() => deleteParkingSpot(+getParkingSpotId(params))?.then(()=>{updateParkingSpots(); dispatch(SET_SUCCESS_ALERT({status:true, message: "Parkimiskoht kustutatud!"}));})}><XCircle color="#e08d8d"/></Button></Tooltip>
                     </ButtonGroup>;
             }
           },
       ];
 
+      const useStyles = makeStyles((theme: Theme) =>
+      createStyles({
+        modal: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '1200px',
+          margin: 'auto',
+        }
+      }),
+    );
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => { setOpen(true);};
+  const handleClose = () => { setOpen(false);};
 
     return (
       <>
         <div style={{ width: '100%'}}>
         </div>
-        <Box display="flex" justifyContent="flex-end">
-          <Button color="primary" variant="contained">
-            Lisa parklakoht
-          </Button>
-        </Box>
+        <Box display="flex" justifyContent="flex-end"><Button color="primary" variant="contained" />Lisa parklakoht</Box>
+        <Box display="flex" justifyContent="flex-end"><Button color="primary" variant="contained" onClick={handleOpen} />Parklaplaan</Box>
+        <Modal
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div>
+                <CardMedia component="img" image="https://bosch.io/wp-content/uploads/parking-lot-monitoring.jpg" />
+            </div>
+          </Fade>
+        </Modal>
+
         <TextField
           variant="standard"
           onChange={event => {
